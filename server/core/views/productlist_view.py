96488e6b46b841830643cceee942ac7d05b00c49
abdullah-django <git@abdullahzulfiqar.me>
2022-08-06 21:15:30 +0500

    All List view is set with flags

--- server/core/views/productlist_view.py
+++ server/core/views/productlist_view.py
@@ -1,4 +1,4 @@
-from core.models import Product
+from core.models import Product, Productdescriptions,SearchAttribute
 from rest_framework import permissions
 from rest_framework.generics import ListAPIView
 from core.utils import categories_with_all_childs
@@ -17,18 +17,71 @@ class ProductListView(ListAPIView):
         self.all_categories = None
 
     def get_queryset(self):
-        # flag = self.request.query_params.get('flag')
-        # if not flag:
-        #     return Product.objects.none()
+        flag = self.request.query_params.get('flag', None)
         category_id = self.request.query_params.get(
             'categoryid', None)
-        # getting all child categories of the category ids
-        self.all_categories = categories_with_all_childs(category_id)
-        # prefectching the product description, product skus and product elements by using prefetch_related.
-        queryset = Product.objects.filter(
-            categoryid__in=self.all_categories).prefetch_related(
-            'productDescription',
-            'productSkus',
-            'productElements__productElementProperties'
-        )
-        return queryset
+        search = self.request.query_params.get('search', None)
+        value_id = self.request.query_params.get(
+            'valueid', None)
+        manufacturer_id = self.request.query_params.get('manufacturerid', None)
+        
+        if flag.lower()=='manufacturer':
+            # getting all child categories of the category ids
+            self.all_categories = categories_with_all_childs(category_id)
+            # Getting product id from the product table by using manufacturer id .
+            product_ids = Product.objects.filter(
+                manufacturerid=manufacturer_id).values('productid')
+            # Getting the product query set by using the filtered product id.
+            queryset = Product.objects.filter(productid__in=product_ids,
+                                            categoryid__in=self.all_categories
+                                            ).prefetch_related(
+                'productDescription',
+                'productSkus',
+                'productElements__productElementProperties'
+            )
+            return queryset
+        elif flag.lower() == 'category':
+
+            # getting all child categories of the category ids
+            self.all_categories = categories_with_all_childs(category_id)
+            # prefectching the product description, product skus and product elements by using prefetch_related.
+            queryset = Product.objects.filter(
+                categoryid__in=self.all_categories).prefetch_related(
+                'productDescription',
+                'productSkus',
+                'productElements__productElementProperties'
+            )
+            return queryset
+        elif flag.lower() == 'search':
+
+            # getting product id from the product table by using search keyword.
+            product_ids = Productdescriptions.objects.filter(
+                description__icontains=search).values('productid')
+            # getting the product query set by using the filtered product id.
+            # prefectching the product description, product skus and product elements by using prefetch_related.
+            queryset = Product.objects.filter(
+                productid__in=product_ids).prefetch_related(
+                'productDescription',
+                'productSkus',
+                'productElements__productElementProperties'
+            )
+            return queryset
+
+        elif flag.lower() == 'productType':
+
+            # getting product id from the Search attribute  table by using valueid.
+            product_ids = SearchAttribute.objects.filter(
+                valueid=value_id).values('productid')
+            # getting category and child categories of the category ids
+            self.all_categories = categories_with_all_childs(category_id)
+            # getting the product query set by using the filtered product id and category_id.
+            # prefectching the product description, product skus and product elements by using prefetch_related.
+            queryset = Product.objects.filter(productid__in=product_ids, categoryid__in=self.all_categories
+                                            ).prefetch_related(
+                'productDescription',
+                'productSkus',
+                'productElements__productElementProperties'
+            )
+            return queryset
+        else:
+            return Product.objects.none()

88083fab05ed6bf8aa09d721a26aa82f76ece5f9
abdullah-django <git@abdullahzulfiqar.me>
2022-08-31 20:52:54 +0500

    Add image url settings in list product serializer

--- server/core/serializers/productlist_serializer.py
+++ server/core/serializers/productlist_serializer.py
@@ -1,5 +1,6 @@
 from core.models import Product
 from rest_framework import serializers
+from gfksite.settings import etilze_content
 from .productsku_serializer import ProductSkusSerializer
 from .productelement_serializer import ProductElementsSerializer
 from .productdescription_serializer import ProductDescriptionsSerializer
@@ -15,11 +16,20 @@ class ProductListSerializer(serializers.ModelSerializer):
     productElements = ProductElementsSerializer(many=True)
     productDescription = ProductDescriptionsSerializer(
         many=True, read_only=True)
+    image_url = serializers.SerializerMethodField()
+
+    def get_image_url(self, obj):
+
+        if etilze_content:
+            return "https://content.etilize.com/images/60/{}.jpg".format(obj.productid)
+        else:
+            return "http://localhost:8000/media/{}.jpg".format(obj.productid)
 
     class Meta:
         model = Product
         fields = [
             'productid',
+            'image_url',
             'mfgpartno',
             'productSkus',
             'productElements',

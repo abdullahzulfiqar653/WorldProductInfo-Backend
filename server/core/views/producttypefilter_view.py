9f734204dc3f910bdebb55e335f8408e929c4ab2
abdullah-django <git@abdullahzulfiqar.me>
2022-09-14 11:45:52 +0500

    Product Type Filter now is properly working

--- server/core/views/producttypefilter_view.py
+++ server/core/views/producttypefilter_view.py
@@ -1,4 +1,4 @@
-from django.db.models import Count
+from django.db.models import Count, Q
 from rest_framework import permissions
 from rest_framework.generics import ListAPIView
 from core.utils import categories_with_all_childs
@@ -8,7 +8,7 @@ from core.models import Product, SearchAttribute, SearchAttributeValues, Categor
 
 class ProductTypeFilterNames(ListAPIView):
 
-    """Listing product filter names for left dsiplayed filters on product 
+    """Listing product filter names for left dsiplayed filters on product
     listing page from the SearchAttributeValues table."""
 
     permission_classes = [permissions.AllowAny]
@@ -21,7 +21,7 @@ class ProductTypeFilterNames(ListAPIView):
         all_categories = categories_with_all_childs(category_id)
         # getting productIds from product table by using filtered categoryIds
         product_ids = Product.objects.filter(
-            categoryid__in=all_categories).values('productid').distinct()
+            categoryid__in=all_categories,).values('productid').distinct()
         # getting valueIds from searchAttributeTable by using filtered productIds
         values_ids = SearchAttribute.objects.filter(
             productid__in=product_ids, attributeid__name='Product Type').values('valueid')
@@ -32,6 +32,7 @@ class ProductTypeFilterNames(ListAPIView):
         3: prefetch_related to pre load searchAttributeValue."""
         queryset = SearchAttributeValues.objects.filter(
             valueid__in=values_ids).annotate(
-                product_count=Count('searchAttributeValue__productid')
-        ).prefetch_related('searchAttributeValue').order_by("-product_count")
+                product_count=Count('searchAttributeValue__productid',
+                                    filter=(Q(searchAttributeValue__productid__categoryid__in=all_categories,)))).prefetch_related(
+                                        'searchAttributeValue').order_by("-product_count")
         return queryset
